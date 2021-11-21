028c2e10f46332294131832442d7dcbd2d5eaf11
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-11-21 12:00:00 -0600

    lodash works

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Chess } from "chess.js";
 import { hello } from "./hello";
+import * as _ from "lodash";
 
 let board = new Chess();
 board.move("e4");
 console.log(board.ascii());
+console.log(_.sum([1, 10, 100]));
